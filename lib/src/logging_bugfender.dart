a0b3eab0bca03d2d10fbd1c088e441103ec411fe
Bartek Pacia <barpac02@gmail.com>
2021-12-10 12:25:56 +0100

    set version to 3.0.0. Drop backwards compatibility

--- lib/src/logging_bugfender.dart
+++ lib/src/logging_bugfender.dart
@@ -14,34 +14,32 @@ class LoggingBugfenderListener {
   /// The `apiUri` and `baseUri` are the alternative URLs for on-premise
   /// installations.
   ///
-  /// The `maximumLocalStorageSize` is a maximum size the log cache will use,
-  /// in bytes.
+  /// The `maximumLocalStorageSize` is a maximum size the log cache will use, in
+  /// bytes.
   ///
-  /// The `printToConsole` decides whether to print to console or not.
-  /// Defaults to `true`.
+  /// The `consolePrintStrategy` decides if and how logs are printed to the
+  /// console.
   ///
-  /// The `printStrategy` decides if and how logs are printed to the console.
-  /// It it is set, then printToConsole should be set to `false` to avoid
-  /// logging everything twice.
+  /// The 'bugfenderPrintStrategy` decides if and how logs are sent to
+  /// Bugfender.
   ///
   /// The `sendToBugfender` decides whether to send logs to bugfender. If it is
   /// set to false, then logs will be printed only locally, to your console.
   ///
-  /// The `enableUIEventLogging` enables automatic logging of user
-  /// interactions. Defaults to `true`.
+  /// The `enableUIEventLogging` enables automatic logging of user interactions.
+  /// Defaults to `true`.
   ///
   /// The `enableCrashReporting` enables automatic crash reporting. Defaults to
   /// `true`.
   ///
-  /// The `enableAndroidLogcatLogging` enables automatic logging of Logcat
-  /// (only on Android). Defaults to `true`. This may result in logging many
+  /// The `enableAndroidLogcatLogging` enables automatic logging of Logcat (only
+  /// on Android). Defaults to `true`. This may result in logging many
   /// unrelevant records.
   LoggingBugfenderListener(
     String appKey, {
     Uri? apiUri,
     Uri? baseUri,
     int? maximumLocalStorageSize,
-    @Deprecated('Use printStrategy instead') bool printToConsole = true,
     this.consolePrintStrategy = const NeverPrintStrategy(),
     this.bugfenderPrintStrategy = const PlainTextPrintStrategy(),
     bool enableUIEventLogging = true,
@@ -53,7 +51,7 @@ class LoggingBugfenderListener {
       apiUri: apiUri,
       baseUri: baseUri,
       maximumLocalStorageSize: maximumLocalStorageSize,
-      printToConsole: printToConsole,
+      printToConsole: false, // we do logging ourselves
       enableUIEventLogging: enableUIEventLogging,
       enableCrashReporting: enableCrashReporting,
       enableAndroidLogcatLogging: enableAndroidLogcatLogging,
@@ -65,12 +63,11 @@ class LoggingBugfenderListener {
 
   /// Defines if and how logs should be created and sent to Bugfender.
   ///
-  /// Using [ColoredTextPrintStrategy] for this might have unexpected results
-  /// and is discouraged.
+  /// For this, [ColoredTextPrintStrategy] works the same as
+  /// [PlainTextPrintStrategy].
   final PrintStrategy bugfenderPrintStrategy;
 
-  /// Registers a [Logger] listener to the Bugfender.
-  /// Starts listening to logs emitted by [logger]
+  /// Starts listening to logs emitted by [logger].
   StreamSubscription<LogRecord> listen(Logger logger) {
     return logger.onRecord.listen((record) {
       if (consolePrintStrategy is NeverPrintStrategy &&
@@ -85,6 +82,7 @@ class LoggingBugfenderListener {
       } else if (consolePrintStrategy is ColoredTextPrintStrategy) {
         final log = _createLog(record);
         // TODO: make log colorful
+        // ignore: avoid_print
         print(log);
       }
 
